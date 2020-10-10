a48767bf029b18d75092f79f28bcb58b7a07a39b
mariamdashti-c34 <70545845+mariamdashti-c34@users.noreply.github.com>
2020-10-10 20:16:39 +0300

    donee

--- scripts.js
+++ scripts.js
@@ -1,13 +1,32 @@
 // 1
 // moon.jpg قم بالوصول إلى الصورة وتغيرها إلى
-
+document.getElementById("image") 
+.src="./image/moon.jpg"
 // 2
 // "قم بالوصول إلى كلمة الأرض وتحويلها إلى "القمر
-
+document.getElementById("caption");
+console.log(title.innerHTML);
+title.innerHTML="الشمس";
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+function planetJump(){
+    let image = document.getElementById("image");
 
+    let title = document.getElementById("caption");
+let body=document.getElementsByTagName("body")[0]
+
+    if (title.innerHTML=="القمر"){
+        title.innerHTML="الشمس";
+        image.src="./images/sun.jpg"
+        body.style.background="//#endregion182848"
+    }
+    else{
+        title.innerHTML="القمر"
+        image.src="./images/moon.jpg";
+        body.style.background="grey"
+    }
+}
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
 
